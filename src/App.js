f219165b5cb86f162f086e240956398d4c93f727
Alexandr Kulish <81294507+Darykz@users.noreply.github.com>
2022-01-31 10:47:52 +0200

    html code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-
+import React from "react";
+import List from "./components/List";
 
 function App() {
-  return (
-    <h1>some text</h1>      
-  );
+  return <div className="todo">
+    <div className="todo__sidebar">
+      <List></List>
+    </div>
+    <div className="todo__task">somelittle</div>
+  </div>; 
 }
 
 export default App;
